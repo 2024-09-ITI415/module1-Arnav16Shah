bc9a53ab66f3eb29583fcd248c81e2cd9b522967
Arnav16Shah <ans228@scarletmail.rutgers.edu>
2024-10-10 19:49:04 -0400

    1st changes
    1st

--- Assets/01-Apple Picker/ApplePicker.cs
+++ Assets/01-Apple Picker/ApplePicker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -14,20 +15,40 @@ public class ApplePicker : MonoBehaviour
 
     public float basketSpacingY = 2f;
 
+    public List<GameObject> basketList;
+
     // Start is called before the first frame update
     void Start()
     {
+        basketList = new List<GameObject>();
         for (int i=0; i<numBaskets; i++)
         {
             GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
            
         }
+
     }
 
-    // Update is called once per frame
+    public void AppleDestroyed()
+    {
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+
+        foreach (GameObject tGO in tAppleArray)
+        {
+            Destroy(tGO);
+        }
+
+        int basketIndex = basketList.Count - 1;
+
+        GameObject tBasketGO = basketList[basketIndex];
+
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
+    }
     void Update()
     {
         
--- Assets/01-Apple Picker/Scripts/Apple.cs
+++ Assets/01-Apple Picker/Scripts/Apple.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Apple : MonoBehaviour
 {
+    [Header("Set in Inspector")]
     public static float bottomY = -20f;
     
     // Update is called once per frame
@@ -11,7 +12,11 @@ public class Apple : MonoBehaviour
     {
         if ( transform.position.y < bottomY)
         {
-            Destroy(this.gameObject);
+            Destroy( this.gameObject );
+
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+
+            apScript.AppleDestroyed();
         }
     }
 }

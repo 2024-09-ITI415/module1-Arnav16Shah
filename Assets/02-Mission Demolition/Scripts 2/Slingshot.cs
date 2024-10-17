9392511f10b83f3cd29b14171cf8e40dcf194bb9
Arnav16Shah <ans228@scarletmail.rutgers.edu>
2024-10-17 18:04:38 -0400

    class changes/mission dem finished

--- Assets/02-Mission Demolition/Scripts 2/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts 2/Slingshot.cs
@@ -92,6 +92,7 @@ public class Slingshot : MonoBehaviour
             projectileRigidbody.velocity = -mouseDelta * velocityMult;
             FollowCam.POI = projectile;
             projectile = null;
+          
         }
 
 
